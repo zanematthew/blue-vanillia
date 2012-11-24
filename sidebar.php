04389ac2c9445976c0e033a9bd5d33fbe34c5ce2
Zane M. Kolnik <zanematthew@gmail.com>
2012-11-24 12:47:59 -0500

    adding content to sidebar

--- sidebar.php
+++ sidebar.php
@@ -7,24 +7,35 @@
             </div>
         <?php endif; ?>
 
+        <div class="zm-base-list-terms-container">
+            <div class="zm-base-item">
+                <div class="zm-base-title">Events</div>
+                <?php foreach( get_terms('type') as $type ) : ?>
+                    <div class="zm-base-item">
+                        <?php if ( get_query_var('term') == $type->slug ) $class = 'current'; else $class = null; ?>
+                        <a href="<?php print get_term_link($type->slug, 'type'); ?>" class="<?php print $class; ?>"><?php print $type->name; ?></a>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+
         <?php if ( get_option('zm_geo_location_version') ) zm_geo_location_current_location_target(); ?>
+            <?php
 
-<?php
+            global $post;
+            $region = get_post_meta( $post->ID, 'venues_state', true );
 
-global $post;
-$region = get_post_meta( $post->ID, 'venues_state', true );
-
-if ( empty( $region ) ){
-    $region = "Maryland";
-}
-$tmp = new Venues; if ( $tmp->getVenueByRegion() ) : ?>
-    <div class="zm-base-list-terms-container">
-        <div class="zm-base-title">Venues</div>
-        <?php foreach( $tmp->getVenueByRegion( $region ) as $venue ): ?>
-            <div class="zm-base-item"><a href="<?php print get_permalink( $venue->ID ); ?>"><?php print $venue->post_title; ?></a><br /></div>
-        <?php endforeach; ?>
-    </div>
-<?php endif; ?>
+            if ( empty( $region ) ){
+                $region = "MD";
+            }
+            $tmp = new Venues; if ( $tmp->getVenueByRegion() ) : ?>
+            <div class="zm-base-list-terms-container">
+                <div class="zm-base-title">Venues</div>
+                <?php foreach( $tmp->getVenueByRegion( $region ) as $venue ): ?>
+                    <div class="zm-base-item"><a href="<?php print get_permalink( $venue->ID ); ?>"><?php print $venue->post_title; ?></a><br /></div>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
 
         <!-- Share -->
         <?php if ( get_option( 'zm_social_version' ) ) : ?>
