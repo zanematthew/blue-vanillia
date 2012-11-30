385026fa4c588f04b4180b1b9895bd1171f84a1f
Zane M. Kolnik <zanematthew@gmail.com>
2012-11-29 20:02:29 -0500

    fixing conditional in tabs

--- events-upcoming.php
+++ events-upcoming.php
@@ -9,15 +9,19 @@ $next_month_events = $event_obj->getMonth( $plus_one_month );
 
 $plus_three_month = date( 'M', strtotime( date( "Y-m-d", strtotime( date("Y-m-d") ) ) . "+2 month" ) );
 $three_months_out = $event_obj->getMonth( $plus_three_month );
+
 ?>
 <div class="row">
     <h2 class="title">Upcoming Events</h2>
     <div class="tabs-container tabs-handle">
+
         <ul>
             <li><a href="#locals-current-month"><?php print date('M'); ?> <span class="count"><?php print $current_events['count']; ?></span></a></li>
-            <li><a href="#locals-next-month"><?php print $plus_one_month; ?> <span class="count"><?php print $next_month_events['count']; ?></span></a></li>
+            <li><a href="#demo">demo</a></li>
+            <?php if ( $next_month_events ) : ?><li><a href="#locals-next-month"><?php print $plus_one_month; ?> <span class="count"><?php print $next_month_events['count']; ?></span></a></li><?php endif; ?>
             <?php if ( $three_months_out ) : ?><li><a href="#locals-next-next-month"><?php print $plus_three_month; ?> <span class="count"><?php print $three_months_out['count']; ?></span></a></li><?php endif; ?>
         </ul>
+
         <?php if ( $current_events ) : ?>
             <div id="locals-current-month">
                 <div class="row-container">
