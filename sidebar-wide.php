d44c179ea0457515c3a18542e7bfbf5ae568ebf9
Zane M. Kolnik <zanematthew@gmail.com>
2012-12-03 18:31:13 -0500

    only display tabs if we have content

--- sidebar-wide.php
+++ sidebar-wide.php
@@ -6,7 +6,7 @@
                     With over <strong><?php Events::eventCount(); ?></strong> and <strong><?php Venues::venueCount(); ?></strong> listed you're sure to find an event to attend!
                     <br>
                     <a href="/?s=%2B&quot;Maryland&quot;&amp;post_type=events" class="button" data-original-title="">View All Events</a>
-                    <a href="/?s=&quot;Maryland&quot;&amp;post_type=venues" class="button last" data-original-title="">View All Tracks</a>
+                    <a href="/?s=&quot;Maryland&quot;&amp;post_type=venues" class="button last" data-original-title="">View All Venues</a>
                 </div>
             </div>
         </div>
