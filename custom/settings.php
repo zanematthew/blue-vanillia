a9155d6449051ae6e2fddaaf560833701874000f
Zane M. Kolnik <zanematthew@gmail.com>
2013-01-29 20:44:57 -0500

    Adding button pane, updating to use new params for multiselect.
    Signed-off-by: Zane M. Kolnik <zanematthew@gmail.com>

--- custom/settings.php
+++ custom/settings.php
@@ -5,11 +5,11 @@
 global $current_user;
 get_currentuserinfo();
 $venues = New Venues;
-
+$events = New Events;
 ?>
 <div class="zm-ev-settings">
     <div class="main-container">
-        <form action="" method="POST" id="zm_ev_settings_form">
+        <form action="" method="POST" id="zm_ev_settings_form" class="zm-form">
             <div class="row">
                 <label>Default Location</label>
                 <input type="text" name="default_location" placeholder="21045" />
@@ -18,13 +18,16 @@ $venues = New Venues;
             <div class="row">
                 <label>View only the following Events</label>
                 <?php $venues->stateSelect( array( 'current' => get_user_meta( $current_user->ID, 'zm_state_preference', true ),'multiple'=>true) ); ?>
-                <?php Events::typeSelectBox( get_user_meta( $current_user->ID, 'zm_type_preference', true ) ); ?>
-                <?php print $venues->locationSelect( array( 'current'=>get_user_meta( $current_user->ID, 'zm_venue_preference', true ),'multiple'=>true ) ); ?>
+                <?php $events->typeSelectBox( get_user_meta( $current_user->ID, 'zm_type_preference', true ) ); ?>
+                <?php print $venues->locationSelect( get_user_meta( $current_user->ID, 'zm_venue_preference', true ) ); ?>
             </div>
 
             <div class="row">
                 <label>Email</label>
                 <input type="text" name="user_email" placeholder="<?php print $current_user->user_email; ?>" />
+            </div>
+            <div class="button-pane">
+                <input type="submit" value="Save" />
             </div>
         </form>
     </div>
