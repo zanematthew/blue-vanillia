c0dec8952347dc64fb29e740a4a630e716f00864
Zane M. Kolnik <zanematthew@gmail.com>
2013-02-02 17:25:07 -0500

    css fixes, added user message function.
    Signed-off-by: Zane M. Kolnik <zanematthew@gmail.com>

--- header.php
+++ header.php
@@ -40,6 +40,8 @@ else
 //
 ?>
 <div class="header-container">
+    <?php zm_ev_user_pref_message( get_current_user_id() ); ?>
+    <div class="border"></div>
     <div class="logo-container">
         <hgroup>
             <h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
